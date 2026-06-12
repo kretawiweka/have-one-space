5a5ffbf11a63ce8b669dfc96d58dea28c3f4dd0f
Caramel Macchiato <ekanuraga@gmail.com>
2026-06-12 11:16:42 +0700

    fix: hide about page (#5)
    Co-authored-by: kretawiweka.sani <kretawiweka.sani@gojek.com>

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -54,41 +54,41 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             </div>
           </header>
           <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
-            <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
-              <dt className="sr-only">Authors</dt>
-              <dd>
-                <ul className="flex justify-center space-x-8 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
-                  {authorDetails.map((author) => (
-                    <li className="flex items-center space-x-2" key={author.name}>
-                      {author.avatar && (
-                        <Image
-                          src={author.avatar}
-                          width={38}
-                          height={38}
-                          alt="avatar"
-                          className="h-10 w-10 rounded-full"
-                        />
-                      )}
-                      <dl className="whitespace-nowrap text-sm font-medium leading-5">
-                        <dt className="sr-only">Name</dt>
-                        <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
-                        <dd>
-                          {author.twitter && (
-                            <Link
-                              href={author.twitter}
-                              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            >
-                              {author.twitter.replace('https://twitter.com/', '@')}
-                            </Link>
-                          )}
-                        </dd>
-                      </dl>
-                    </li>
-                  ))}
-                </ul>
-              </dd>
-            </dl>
+            {/*<dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">*/}
+            {/*  <dt className="sr-only">Authors</dt>*/}
+            {/*  <dd>*/}
+            {/*    <ul className="flex justify-center space-x-8 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">*/}
+            {/*      {authorDetails.map((author) => (*/}
+            {/*        <li className="flex items-center space-x-2" key={author.name}>*/}
+            {/*          {author.avatar && (*/}
+            {/*            <Image*/}
+            {/*              src={author.avatar}*/}
+            {/*              width={38}*/}
+            {/*              height={38}*/}
+            {/*              alt="avatar"*/}
+            {/*              className="h-10 w-10 rounded-full"*/}
+            {/*            />*/}
+            {/*          )}*/}
+            {/*          <dl className="whitespace-nowrap text-sm font-medium leading-5">*/}
+            {/*            <dt className="sr-only">Name</dt>*/}
+            {/*            <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>*/}
+            {/*            <dt className="sr-only">Twitter</dt>*/}
+            {/*            <dd>*/}
+            {/*              {author.twitter && (*/}
+            {/*                <Link*/}
+            {/*                  href={author.twitter}*/}
+            {/*                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"*/}
+            {/*                >*/}
+            {/*                  {author.twitter.replace('https://twitter.com/', '@')}*/}
+            {/*                </Link>*/}
+            {/*              )}*/}
+            {/*            </dd>*/}
+            {/*          </dl>*/}
+            {/*        </li>*/}
+            {/*      ))}*/}
+            {/*    </ul>*/}
+            {/*  </dd>*/}
+            {/*</dl>*/}
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
             </div>
